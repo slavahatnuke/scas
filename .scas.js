e977a9b9ee7622df44e5f2fe88e6bc0470b83193
slava hatnuke <slava.hatnuke@gmail.com>
2017-01-15 20:07:35 +0200

    wip

--- .scas.js
+++ .scas.js
@@ -42,45 +42,5 @@ it will give you some exmaplesn it will give you some exmaples
             input: 'sandbox/es6/class.js',
             out: '{{{ dir }}}/{{{ name | Camel }}}.js'
         }
-    },
-    pipes: {
-        camel: (context) => (input) => _.camelCase(input),
-        Camel: (context) => (input) => _.upperFirst(_.camelCase(input)),
-
-        kebab: (context) => (input) => _.kebabCase(input),
-        Kebab: (context) => (input) => _.upperFirst(_.kebabCase(input)),
-
-        snake: (context) => (input) => _.snakeCase(input),
-        Snake: (context) => (input) => _.upperFirst(_.snakeCase(input)),
-
-        lower: (context) => (input) => _.toLower(input),
-        upper: (context) => (input) => _.toUpper(input),
-
-        toUpper: (context) => (input) => _.toUpper(input),
-        toLower: (context) => (input) => _.toLower(input),
-
-        camelCase: (context) => (input) => _.camelCase(input),
-        kebabCase: (context) => (input) => _.kebabCase(input),
-        snakeCase: (context) => (input) => _.snakeCase(input),
-        startCase: (context) => (input) => _.startCase(input),
-
-        lowerCase: (context) => (input) => _.lowerCase(input),
-        upperCase: (context) => (input) => _.upperCase(input),
-
-        lowerFirst: (context) => (input) => _.lowerFirst(input),
-        upperFirst: (context) => (input) => _.upperFirst(input),
-
-        capitalize: (context) => (input) => _.capitalize(input),
-        trim: (context) => (input) => _.trim(input),
-
-        trimStart: (context) => (input) => _.trimStart(input),
-        trimEnd: (context) => (input) => _.trimEnd(input),
-
-        plural: (context) => (input) => pluralize.plural(input),
-        Plural: (context) => (input) => _.upperFirst(pluralize.plural(input)),
-
-        singular: (context) => (input) => pluralize.singular(input),
-        Singular: (context) => (input) => _.upperFirst(pluralize.singular(input))
-
     }
 };
--- src/workspace/pipe/Pipes.js
+++ src/workspace/pipe/Pipes.js
@@ -2,30 +2,32 @@ let _ = require('lodash');
 
 module.exports = class Pipes {
     constructor() {
-        this.pipes = {}
+        this.pipes = {};
     }
 
     load(context) {
         return Promise.resolve()
-            .then(() => {
-                let pipes = context.config.pipes || {};
-                _.each(pipes, (pipe, name) => {
-                    this.pipes[name] = (input) => {
-                        if (_.isFunction(pipe)) {
-                            let _pipe = pipe(context);
-
-                            if (_.isFunction(_pipe)) {
-                                return _pipe(input);
-                            } else {
-                                return '--wrong pipe--';
-                            }
-                        }
-
-                        return '--wrong pipe declaration--';
-                    }
-                });
-            })
+            .then(() => this.loadPipes(context, require('../../../pipes/pipes')))
+            .then(() => this.loadPipes(context, context.config.pipes || {}))
             .then(() => this);
+    }
+
+    loadPipes(context, pipes) {
+        _.each(pipes, (pipe, name) => {
+            this.pipes[name] = (input) => {
+                if (_.isFunction(pipe)) {
+                    let _pipe = pipe(context);
+
+                    if (_.isFunction(_pipe)) {
+                        return _pipe(input);
+                    } else {
+                        return '--wrong pipe--';
+                    }
+                }
+
+                return '--wrong pipe declaration--';
+            }
+        });
     }
 
     getPipes() {
